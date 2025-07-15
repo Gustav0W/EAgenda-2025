6c6ef7f0acea6e60da555830d5cca4f9cc50fa35
GustavoW <guvieirawalter@gmail.com>
2025-07-15 15:39:58 -0300

    Implementado ActionFilters

--- EAgenda.WebApp/ActionFilters/ValidarModeloAttribute.cs
+++ EAgenda.WebApp/ActionFilters/ValidarModeloAttribute.cs
@@ -1,6 +1,22 @@
-﻿namespace EAgenda.WebApp.ActionFilters
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EAgenda.WebApp.ActionFilters;
+public class ValidarModeloAttribute : ActionFilterAttribute
 {
-    public class ValidarModeloAttribute
+    public override void OnActionExecuting(ActionExecutingContext context)
     {
+        var modelState = context.ModelState;
+
+        if (!modelState.IsValid)
+        {
+            var controller = (Controller)context.Controller;
+
+            var viewModel = context.ActionArguments
+                .Values
+                .FirstOrDefault(x => x.GetType().Name.EndsWith("ViewModel"));
+
+            context.Result = controller.View(viewModel);
+        }
     }
 }
