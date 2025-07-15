14debd40333e3a357517fe9d42325b42589b68ce
GustavoW <guvieirawalter@gmail.com>
2025-07-15 15:48:07 -0300

    Instalado pacote serilog no webapp

--- EAgenda.WebApp/DependencyInjection/SerilogConfig.cs
+++ EAgenda.WebApp/DependencyInjection/SerilogConfig.cs
@@ -1,6 +1,22 @@
-﻿namespace EAgenda.WebApp.DependencyInjection
+﻿using Serilog;
+
+namespace EAgenda.WebApp.DependencyInjection;
+
+public static class SerilogConfig
 {
-    public class SerilogConfig
+    public static void AddSerilogConfig(this IServiceCollection services, ILoggingBuilder logging)
     {
+        var caminhoAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        var caminhoArquivoLogs = Path.Combine(caminhoAppData, "eAgenda", "erro.log");
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File(caminhoArquivoLogs, Serilog.Events.LogEventLevel.Error)
+            .CreateLogger();
+
+        logging.ClearProviders();
+        services.AddSerilog();
     }
 }
