8bdef6c73dc09af8d5ba8e9ed769d7161432dc0c
GustavoW <guvieirawalter@gmail.com>
2025-07-15 22:46:29 -0300

    Feitas últimas migrations

---
+++ EAgenda.Infraestrutura.Orm/Migrations/20250716014519_Add_TBItensTarefa.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EAgenda.Infraestrutura.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_TBItensTarefa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ItensTarefa",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Concluido = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensTarefa", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItensTarefa");
+        }
+    }
+}
