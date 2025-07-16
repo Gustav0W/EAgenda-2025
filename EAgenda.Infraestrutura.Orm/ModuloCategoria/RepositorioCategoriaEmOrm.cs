69b425e40ad0590e882a0b0881e630ad78c557c7
GustavoW <guvieirawalter@gmail.com>
2025-07-15 22:12:16 -0300

    Refatorado repositorio categoria em orm

--- EAgenda.Infraestrutura.Orm/ModuloCategoria/RepositorioCategoriaEmOrm.cs
+++ EAgenda.Infraestrutura.Orm/ModuloCategoria/RepositorioCategoriaEmOrm.cs
@@ -3,37 +3,9 @@ using EAgenda.Infraestrutura.Orm.Compartilhado;
 
 namespace EAgenda.Infraestrutura.Orm.ModuloCategoria;
 
-public class RepositorioCategoriaEmOrm : IRepositorioCategoria
+public class RepositorioCategoriaEmOrm :  RepositorioBaseEmOrm<Categoria>, IRepositorioCategoria
 {
-    public readonly EAgendaDbContext contexto;
+    public RepositorioCategoriaEmOrm(EAgendaDbContext contexto) : base(contexto) { }
 
-    public RepositorioCategoriaEmOrm(EAgendaDbContext contexto) 
-    {
-        this.contexto = contexto;
-    }
-
-    public void CadastrarRegistro(Categoria novoRegistro)
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool EditarRegistro(Guid idRegistro, Categoria registroEditado)
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool ExcluirRegistro(Guid idRegistro)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Categoria SelecionarRegistroPorId(Guid idRegistro)
-    {
-        throw new NotImplementedException();
-    }
-
-    public List<Categoria> SelecionarRegistros()
-    {
-        throw new NotImplementedException();
-    }
+    
 }
